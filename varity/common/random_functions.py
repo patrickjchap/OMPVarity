b0ffea3544279b6843e6e1e31f7911a3f036672d
Patrick <pchapman@ucdavis.edu>
2023-08-23 10:46:08 -0700

    Adding a randomListChunk function

--- varity/common/random_functions.py
+++ varity/common/random_functions.py
@@ -8,6 +8,16 @@ def lucky():
 def veryLucky():
     return random.randrange(0, 5) == 4
 
+# Splits list into n random chunks.
+def randomListChunk(list_to_split, n):
+    ret = [[] for i in range(n)]
+    for v in list_to_split:
+        ret[random.randrange(0,n)].append(v)
+
+    #print(list_to_split, " ---> ", ret)
+    return ret
+
+
 # Chance of generating a math function
 def generateMathExpression():
     return random.random() <= cfg.MATH_FUNC_PROBABILITY
